f420ed1a17198f9a9c1cc4659d7f586e22b48015
karl <enriquezkharl14@gmail.com>
2025-03-08 23:55:23 +0800

    [Feat] Player rotation

--- player.go
+++ player.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"go-asteroids/assets"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+const rotationPerSecond = math.Pi
+
 type Player struct {
-	sprite *ebiten.Image
+	sprite   *ebiten.Image
+	rotation float64
 }
 
 func NewPlayer(game *Game) *Player {
@@ -17,11 +21,25 @@ func NewPlayer(game *Game) *Player {
 }
 
 func (p *Player) Draw(screen *ebiten.Image) {
+	bounds := p.sprite.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+
 	opt := &ebiten.DrawImageOptions{}
+
+	opt.GeoM.Translate(-halfW, -halfH)
+	opt.GeoM.Rotate(p.rotation)
+	opt.GeoM.Translate(halfW, halfH)
 
 	screen.DrawImage(p.sprite, opt)
 }
 
 func (p *Player) Update() {
+	speed := rotationPerSecond / float64(ebiten.TPS())
 
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		p.rotation -= speed
+	} else if ebiten.IsKeyPressed(ebiten.KeyD) {
+		p.rotation += speed
+	}
 }
